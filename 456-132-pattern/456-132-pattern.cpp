e6552b29a1a4df876c592f21da6745f1241213a4
Mou3098 <mostarinabinre@gmail.com>
2022-05-19 22:46:01 +0600

    Time: 76 ms (89.13%), Space: 39.2 MB (18.18%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -10,10 +10,8 @@ public:
       }
       stack <int> s;
       for(int i = n - 1; i > 0; i--){
-         int minVal = minVals[i - 1];
-         int curr = nums[i];
-         while(!s.empty() && s.top() <= minVal) s.pop();
-         if(!s.empty() && s.top() < curr) return true;
+         while(!s.empty() && s.top() <= minVals[i - 1]) {s.pop();}
+         if(!s.empty() && s.top() < nums[i]) {return true;}
          s.push(nums[i]);
       }
       return false;
