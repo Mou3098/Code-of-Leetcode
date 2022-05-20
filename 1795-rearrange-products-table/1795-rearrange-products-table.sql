7f50d6aa90bb6e287d65c729b22e35bd018b4984
Mou3098 <mostarinabinre@gmail.com>
2022-05-20 13:00:16 +0600

    Time: 518 ms (59.64%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
-select * from 
-(
-    select product_id, 'store1' as store, store1 price from Products
-    union 
-    select product_id, 'store2' as store, store2 price from Products
-    union
-    select product_id, 'store3' as store, store3 price from Products
-)t
-where price is not null;
+(select product_id, 'store1' as store, store1 price from Products
+where store1 is not null)
+union 
+select product_id, 'store2' as store, store2 price from Products
+where store2 is not null
+union
+select product_id, 'store3' as store, store3 price from Products
+where store3 is not null;
