2bbc8dc634ecc1e905dadd3a08c2c75adc43591f
Mou3098 <mostarinabinre@gmail.com>
2022-05-21 22:36:01 +0600

    Time: 0 ms (100.00%), Space: 6.2 MB (13.85%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string interpret(string command) {
+        int n=command.size();
+        string ans;
+        for(int i=0;i<n;i++)
+        {
+            if(command[i]==')')
+            {
+                continue;
+            }
+            if(command[i]=='(')
+            {
+                if(command[i+1]==')')
+                {
+                    ans+='o';
+                }
+            }
+            else
+            {
+                ans+=command[i];
+            }
+                
+        }
+        return ans;
+    }
+};
