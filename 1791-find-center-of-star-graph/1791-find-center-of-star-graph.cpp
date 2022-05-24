f8718227013cce3d79659dcb5bb5a020500871a8
Mou3098 <mostarinabinre@gmail.com>
2022-05-24 23:18:03 +0600

    Time: 381 ms (18.12%), Space: 69.5 MB (31.85%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -3,14 +3,15 @@ public:
     int findCenter(vector<vector<int>>& edges) {
         int n=edges.size();
         vector<int> v;
+        vector<int> v1;
         sort(edges[0].begin(), edges[0].end());
         sort(edges[1].begin(), edges[1].end());
         set_intersection(edges[0].begin(),edges[0].end(),
                           edges[1].begin(),edges[1].end(),
-                          back_inserter(v));
+                          back_inserter(v1));
         for(int i=1;i<n;i++)
         {
-            set_intersection(edges[i-1].begin(),edges[i-1].end(),
+            set_intersection(v1.begin(),v1.end(),
                           edges[i].begin(),edges[i].end(),
                           back_inserter(v));
         }
