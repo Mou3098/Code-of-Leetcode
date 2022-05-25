2a5ab46c8b749eb8dd267479a8bf4cd715c99b89
Mou3098 <mostarinabinre@gmail.com>
2022-05-26 01:36:01 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (70.15%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int ans=0;
+        int l=0;
+        while(n>1)
+        {
+            if(l)
+            {
+                ans+=(n/2);
+            }
+            else
+            {
+                ans+=n/2;
+            }
+            if(n%2)
+            {
+                l=1;
+                n=(n/2)+1;
+            }
+            else
+            {l=0;
+            n=n/2;
+            }
+            
+        }
+        return ans;
+    }
+};
