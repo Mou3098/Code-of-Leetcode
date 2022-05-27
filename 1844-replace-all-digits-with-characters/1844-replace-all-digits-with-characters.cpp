207034c891c75a089b12545610c1bb0c160cf8a1
Mou3098 <mostarinabinre@gmail.com>
2022-05-27 10:13:45 +0600

    Time: 4 ms (27.74%), Space: 6.2 MB (6.45%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -5,9 +5,11 @@ public:
         int i,n=s.length();
         for(i=0;i<n;i++)
         {
-            int c = s[i]-'0';
+            
             if(i%2==1)
+            {int c = s[i]-'0';
                 ans+=(char) (s[i-1]+c);
+            }
             else
                 ans+=s[i];
         }
