03685fa668c1188aa0887f163d49d02986130edf
Mou3098 <mostarinabinre@gmail.com>
2022-05-31 21:48:07 +0600

    Time: 1507 ms (5.97%), Space: 76.3 MB (33.96%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        set<string> substr_set;
-        int len = s.size();
-        int no_of_comb = (1<<k);
-        cout<<no_of_comb<<endl;
-        for(int i = 0; i+k <= len ; i++){
-            substr_set.insert(s.substr(i,k));
+       set<string> st;
+        int no_of_combination =1<<k;
+        for(int i=0;i+k<=s.size();i++)
+        {
+            st.insert(s.substr(i,k));
         }
-        return substr_set.size() == no_of_comb;
+        return st.size() == no_of_combination;
     }
 };
