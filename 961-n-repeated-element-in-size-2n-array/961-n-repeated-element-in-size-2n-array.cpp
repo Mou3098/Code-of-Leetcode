6d2ac251ca9134212c1625eca1e671dcf247063e
Mou3098 <mostarinabinre@gmail.com>
2022-06-12 16:55:24 +0600

    Time: 48 ms (64.94%), Space: 29.9 MB (39.75%) - LeetHub

--- 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.cpp
+++ 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
         vector<int> v(10000,0);
-        int p=0;
+        int p;
         for(int i=0;i<=(nums.size()/2)+1;i++)
         {
             v[nums[i]]++;
