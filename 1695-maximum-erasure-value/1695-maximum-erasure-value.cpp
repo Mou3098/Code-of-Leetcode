13ce0ade50074600673fa5aa11654d4cd22ea82e
Mou3098 <mostarinabinre@gmail.com>
2022-06-12 16:13:57 +0600

    Time: 902 ms (5.96%), Space: 156.9 MB (7.66%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -3,34 +3,27 @@ public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int n=nums.size();
         int i = 0, j = 1;
-  set<int> set;
- 
-  set.insert(nums[0]);
- 
-  int sum = nums[0];
- 
-  int maxsum = sum;
- 
-  while (i < n - 1 && j < n)
-  {
-    const bool is_in = set.find(nums[j]) ==
-                       set.end();
-    if (is_in)
-    {
-      sum = sum + nums[j];
-      maxsum = max(sum, maxsum);
-      set.insert(nums[j]);
-        j++;
-    }
-
-    else
-    {
-      sum -= nums[i];
-      set.erase(nums[i]);
-        i++;
-    }
-  }
- 
-  return maxsum;
+        set<int> s;
+        s.insert(nums[0]);
+        int sum=nums[0];
+        int maxsum=nums[0];
+        while(i<n-1 &&j<n)
+        {
+            const bool b=s.find(nums[j])==s.end();
+            if(b)
+            {
+                sum+=nums[j];
+                s.insert(nums[j]);
+                maxsum=max(sum,maxsum);
+                j++;
+            }
+            else
+            {
+                 sum-=nums[i];
+                s.erase(nums[i]);
+                i++;
+            }
+        }
+        return maxsum;
     }
 };
