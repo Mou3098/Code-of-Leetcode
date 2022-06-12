6e1391ba83a1c4e0c3ca778e2b43ab0437d30a5d
Mou3098 <mostarinabinre@gmail.com>
2022-06-12 17:41:53 +0600

    Time: 23 ms (16.85%), Space: 7.4 MB (8.48%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,13 +2,37 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        int l = s.length();
-        int fMap [200] = {0};
-        if (t.length() != l) return false;
-        for (int i = 0; i < l; i++)
-            fMap[int(s[i])]++;
-        for (int i = 0; i < l; i++)
-            if (fMap[int(t[i])]-- == 0) return false;
-        return true;
+        map<char, int> f;
+        for(int i=0;i<s.size();i++)
+        {
+            if(f.find(s[i])==f.end())
+            {
+                f.insert(make_pair(s[i], 1));
+            }
+            else
+            {
+                f[s[i]]++;
+            }
+        }
+        map<char, int> f1;
+        for(int i=0;i<t.size();i++)
+        {
+            if(f1.find(t[i])==f1.end())
+            {
+                f1.insert(make_pair(t[i], 1));
+            }
+            else
+            {
+                f1[t[i]]++;
+            }
+        }
+        if (f == f1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 };
