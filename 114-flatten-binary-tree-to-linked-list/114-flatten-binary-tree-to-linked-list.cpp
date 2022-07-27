3d1a9a0abb92f90858a781492b4996fdde2ade3a
Mostarina Mitu <mostarinabinre@gmail.com>
2022-07-27 23:24:54 +0600

    Time: 16 ms (18.16%), Space: 12.5 MB (96.54%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -10,17 +10,29 @@
  * };
  */
 class Solution {
-public:
-   TreeNode* prev;
-    
-    void flatten(TreeNode* root) {
-        if(!root) return;
+    public:
+void flatten(TreeNode* root) {
+  TreeNode* cur = root;
         
-        flatten(root->right);
-        flatten(root->left);
-        
-        root->right = prev;
-        root->left = nullptr;
-        prev = root;
+        while(cur){
+            if(cur->left){
+                TreeNode* prev = cur->left;
+                
+                while(prev->right) prev = prev->right;
+                //now prev is cur's predecessor in in-order traversal
+                //it's also rightmost node of cur's left subtree
+                
+                //let cur's right subtree be cur's left subtree's descendants
+                //prev->right must be nullptr, so this op is safe
+                prev->right = cur->right;
+                //move cur's left subtree to the right
+                cur->right = cur->left;
+                //the reconstructed tree don't have left children
+                cur->left = nullptr;
+            }
+            
+            //move forward in the reconstructed tree
+            cur = cur->right;
+        }
     }
 };
