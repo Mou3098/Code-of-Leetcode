5d1cb731959856654f25d9ae24653bbd5c6c3ca5
Mostarina Mitu <mostarinabinre@gmail.com>
2022-10-28 15:49:40 +0600

    Time: 47 ms (93.95%), Space: 12.3 MB (30.62%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
         int ans = 0;
-        for(int i=0; i<strs[0].length(); i++)
+        for(int i=0; i<strs[0].size(); i++)
         {
             for(int j=1; j<strs.size(); j++)
             {
