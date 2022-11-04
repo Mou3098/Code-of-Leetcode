7f0c2ab3e811a4c83d0da543fe82ec3de5454b24
Mostarina Mitu <mostarinabinre@gmail.com>
2022-11-04 23:15:30 +0600

    Time: 32 ms (47.64%), Space: 17.5 MB (16.86%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        if(matrix.size()==0||matrix.size()==1)
-        {return true;}
         for(int i=0;i<matrix.size()-1;i++)
         {
         for(int j=0;j<matrix[0].size()-1;j++)
