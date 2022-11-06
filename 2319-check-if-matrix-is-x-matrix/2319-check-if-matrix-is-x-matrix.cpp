85db97bd10998f9a71bb687b915db11c860df7d1
Mostarina Mitu <mostarinabinre@gmail.com>
2022-11-06 23:35:21 +0600

    Time: 41 ms (89.45%), Space: 16.2 MB (77.49%) - LeetHub

---
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        for (int i = 0; i < size(grid); ++i) {
+            for (int j = 0; j < size(grid[0]); ++j) {
+                if ((i - j == 0 || i + j == size(grid) - 1) != (grid[i][j] != 0)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
