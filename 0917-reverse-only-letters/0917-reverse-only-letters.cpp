485a207c4004251c1b075999c4ab5ef9319de86e
Mostarina Mitu <mostarinabinre@gmail.com>
2022-11-22 13:56:23 +0600

    Time: 0 ms (100.00%), Space: 6.1 MB (19.44%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i=0,j=s.size()-1;
+        while(j>i)
+        {
+            if(isalpha(s[i])&&isalpha(s[j]))
+            {
+                swap(s[i],s[j]);
+                i++;
+            j--;
+            }
+            else
+            {
+                if (!isalpha(s[i])) i++;
+        if (!isalpha(s[j])) j--;
+            }
+            
+        }
+        return s;
+    }
+};
